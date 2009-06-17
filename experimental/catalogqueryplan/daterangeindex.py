e0ef9633a5f3e0e0b854f9e6d4a634f084abc80c
do3cc <do3cc>
2009-06-17 13:37:25 +0000

    Changed caching so that it works in tests too.

--- experimental/catalogqueryplan/daterangeindex.py
+++ experimental/catalogqueryplan/daterangeindex.py
@@ -6,45 +6,50 @@ from logging import getLogger
 
 logger = getLogger('experimental.catalogqueryplan')
 
-def daterangeindex_apply_index( self, request, cid='', res=None):
-    record = parseIndexRequest( request, self.getId() )
+def daterangeindex_apply_index(self, args, cid='', res=None):
+    record = parseIndexRequest(args, self.getId())
     if record.keys is None:
         return None
 
-    REQUEST = getattr(self, 'REQUEST', None)
-    if REQUEST is not None:
+    try:
+        request = args.request
+    except:
+        request = args.get('request', None)
+    if request is not None:
         requestkey = '_daterangeindex_%s' % self.getId()
-        cached = REQUEST.get(requestkey, None)
-        if cached is not None:
-            return cached, ( self._since_field, self._until_field )
+        cached = request.get(requestkey, None)
 
-    term        = self._convertDateTime( record.keys[0] )
+
+        if cached is not None:
+            return cached, (self._since_field, self._until_field)
+
+    term = self._convertDateTime(record.keys[0])
 
     #
     #   Aggregate sets for each bucket separately, to avoid
     #   large-small union penalties.
     #   XXX Does this apply for multiunion?
     #
-    until_only = multiunion( self._until_only.values( term ) )
+    until_only = multiunion(self._until_only.values(term))
 
-    since_only = multiunion( self._since_only.values( None, term ) )
+    since_only = multiunion(self._since_only.values(None, term))
 
-    until = multiunion( self._until.values( term ) )
+    until = multiunion(self._until.values(term))
 
     # Total result is bound by res
-    if REQUEST is None:
+    if request is None:
         until = intersection(res, until)
 
-    since = multiunion( self._since.values( None, term ) )
+    since = multiunion(self._since.values(None, term))
 
-    bounded     = intersection( until, since )
+    bounded = intersection(until, since)
 
     result = multiunion([bounded, until_only, since_only, self._always])
 
-    if REQUEST is not None:
-        REQUEST.set(requestkey, result)
+    if request is not None:
+        request[requestkey] = result
 
-    return result, ( self._since_field, self._until_field )
+    return result, (self._since_field, self._until_field)
 
 def patch_daterangeindex():
     from Products.PluginIndexes.DateRangeIndex.DateRangeIndex import DateRangeIndex
