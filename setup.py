c29272b7c667b2fe2cc87e1509002e96a0b1a34e
Hanno Schlichting <hanno@jarn.com>
2010-09-24 10:43:38 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from os.path import join
 from setuptools import setup, find_packages, Extension, Feature
 
-version = '3.0.1'
+version = '3.0.2'
 
 base = join('experimental', 'catalogqueryplan')
 
