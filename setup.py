5704719487db125501024903ea0b0b531f196ca1
Hanno Schlichting <hanno@jarn.com>
2011-08-21 00:57:39 +0200

    vb

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from os.path import join
 from setuptools import setup, find_packages, Extension, Feature
 
-version = '3.2.6'
+version = '3.2.7'
 
 base = join('experimental', 'catalogqueryplan')
 
